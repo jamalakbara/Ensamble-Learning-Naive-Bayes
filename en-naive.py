e466259777772a6145445d2cb7416db7ffe0a3af
Akbar <jamalakbaralam@gmail.com>
2019-05-04 06:28:28 +0700

    Beres 1.0

--- en-naive.py
+++ en-naive.py
@@ -1,5 +1,6 @@
 import csv
 import random
+import numpy as np
 from sklearn import preprocessing
 from sklearn.naive_bayes import GaussianNB
 
@@ -38,19 +39,29 @@ def open_file(file):
 
             return x1, x2
 
+def save_file(hasil):
+    with open("TebakanTugas4ML.csv", 'w', newline='') as csv_file:
+        writeCsv = csv.writer(csv_file)
+
+        for row in hasil:
+            writeCsv.writerow([row])
+
 #bikin bag
 def bagging(x1,x2,y):
-    bag = []
+    bag_x1, bag_x2, bag_y = [], [], []
     for i in range(round(len(x1)*0.6)):
         rand_index = random.randint(0,len(x1))
         if rand_index == 298:
             rand_index = 297
-        bag.append([x1[rand_index], x2[rand_index], y[rand_index]])
 
-    return bag
+        bag_x1.append(x1[rand_index])
+        bag_x2.append(x2[rand_index])
+        bag_y.append(y[rand_index])
+
+    return bag_x1, bag_x2, bag_y
 
 #naive bayes nya
-def naive_bayes(x1, x2, y):
+def naive_bayes(x1, x2, y, test1, test2):
     #bikin label encoder
     le = preprocessing.LabelEncoder()
 
@@ -68,11 +79,49 @@ def naive_bayes(x1, x2, y):
     #train model dengan datatrain
     model.fit(features, y_encode)
 
-    predicted = model.predict([[231, 61]])
+    # convert isi dari datatest jadi diskret number
+    test1_encode = le.fit_transform(test1)
+    test2_encode = le.fit_transform(test2)
 
-#bikin 5 bags berbasis buat bagging nya
-# bag1 = bagging(); bag2 = bagging(); bag3 = bagging(); bag4 = bagging(); bag5 = bagging()
+    hasil_prediksi = []
+    #prediksi/klasifikasi naive bayes
+    for i in range(len(test1)):
+        predicted = model.predict([[test1_encode[i], test2_encode[i]]])
+        hasil_prediksi.append(predicted[0])
+
+    return hasil_prediksi
 
 trainset = open_file('TrainsetTugas4ML.csv')
 
+#read testset
+testset = open_file('TestsetTugas4ML.csv')
+
+#bikin 5 bags berbasis naive bayes buat bagging nya
 bag1 = bagging(trainset[0], trainset[1], trainset[2]); bag2 = bagging(trainset[0], trainset[1], trainset[2]); bag3 = bagging(trainset[0], trainset[1], trainset[2]); bag4 = bagging(trainset[0], trainset[1], trainset[2]); bag5 = bagging(trainset[0], trainset[1], trainset[2])
+
+#memasukkan hasil prediksi/klasifikasi naive bayes ke matriks
+matriks = np.matrix(
+    [naive_bayes(bag1[0],bag1[1],bag1[2],testset[0],testset[1]),
+     naive_bayes(bag2[0],bag2[1],bag2[2],testset[0],testset[1]),
+     naive_bayes(bag3[0],bag3[1],bag3[2],testset[0],testset[1]),
+     naive_bayes(bag4[0],bag4[1],bag4[2],testset[0],testset[1]),
+     naive_bayes(bag5[0],bag5[1],bag5[2],testset[0],testset[1])]
+)
+
+#voting dari setiap bags untuk dipilih class/label yg akan dipake
+final_pref = []
+for i in range(len(testset[0])):
+    class1, class2 = 0, 0
+    #ngitung banyakan class yang 2 apa 1
+    for j in range(len(matriks)):
+        if matriks[j, i] == 0:
+            class2 += 1
+        else:
+            class1 += 1
+
+    if class1 > class2:
+        final_pref.append(1)
+    else:
+        final_pref.append(2)
+
+save_file(final_pref)
